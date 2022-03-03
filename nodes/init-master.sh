802613a694aedeffae7857660b26bf4e71ff0e6d
Tom Ruttle <tom.ruttle@thoughtworks.com>
2022-03-04 00:54:18 +0100

    reconfigure for vlan

--- nodes/init-master.sh
+++ nodes/init-master.sh
@@ -1,33 +1,63 @@
-brew install octant
-brew install cilium-cli
-brew install fluxcd/tap/flux
+sudo apt install linux-generic-hwe-20.04
 
-# on master
-kubeadm init --config=/tmp/config.yml --upload-certs
+docker run --network host --rm ghcr.io/kube-vip/kube-vip:$(curl -sL https://api.github.com/repos/kube-vip/kube-vip/releases | jq -r ".[0].name") manifest pod \
+    --interface enx00e04c0814ec \
+    --address 192.168.11.100 \
+    --controlplane \
+    --arp \
+    --leaderElection | tee nodes/manifests/kube-vip.yaml
 
-# on backup
-kubeadm join ...
+scp nodes/manifests/kube-vip.yml katana:/tmp/kube-vip.yml
+scp nodes/kubeadm-config-init.yml katana:/tmp/config.yml
 
-# on machine
+sudo mv /tmp/kube-vip.yml /etc/kubernetes/manifests/
+sudo kubeadm init --config=/tmp/config.yml --upload-certs
+
+# add non-admin user
+
+openssl genrsa -out tom.key 4096
+openssl req -new -key tom.key -out tom.csr
+
+# REMEMBER TO SET COMMON NAME = tom HERE!!!
+
+cat tom.csr | base64 | tr -d '\n'
+
+# add {{ base64_encoded_csr }} to tom-csr.yml
+
+kubectl apply -f nodes/manifests/tom-csr.yml
+kubectl certificate approve tom
+kubectl get csr tom -o jsonpath='{.status.certificate}'| base64 -d > tom.crt
+
+kubectl apply -f nodes/manifests/tom-crb.yml
+
+kubectl config set-credentials tom --client-key=tom.key --client-certificate=tom.crt --embed-certs=true
+kubectl config set-context tom --cluster=kubernetes --user=tom
+kubectl config use-context tom
+
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # install cni
-helm repo add cilium https://helm.cilium.io/
 
-helm install cilium cilium/cilium --version 1.11.1 \
-    --namespace kube-system \
-    --set tunnel=disabled \
-    --set autoDirectNodeRoutes=true \
-    --set hostServices.hostNamespaceOnly=true \
-    --set kubeProxyReplacement=strict \
-    --set loadBalancer.mode=hybrid \
-    --set kubeProxyReplacement=strict \
-    --set k8sServiceHost=192.168.11.100 \
-    --set k8sServicePort=6443 \
-    --set bgp.enabled=true \
-    --set bgp.announce.loadbalancerIP=true \
-    --set hubble.relay.enabled=true \
-    --set hubble.ui.enabled=true
+# helm repo add cilium https://helm.cilium.io/
+
+# helm install cilium cilium/cilium --version 1.11.1 \
+#     --namespace kube-system \
+#     --set tunnel=disabled \
+#     --set autoDirectNodeRoutes=true \
+#     --set hostServices.hostNamespaceOnly=true \
+#     --set kubeProxyReplacement=strict \
+#     --set loadBalancer.mode=hybrid \
+#     --set kubeProxyReplacement=strict \
+#     --set k8sServiceHost=192.168.11.100 \
+#     --set k8sServicePort=6443 \
+#     --set bgp.enabled=true \
+#     --set bgp.announce.loadbalancerIP=true \
+#     --set hubble.relay.enabled=true \
+#     --set hubble.ui.enabled=true
+
+kubectl apply -f nodes/manifests/bgp-config.yml
+
+brew install cilium-cli
 
 cilium install --version -service-mesh:v1.11.0-beta.1 \
     --kube-proxy-replacement=strict \
@@ -42,33 +72,17 @@ cilium install --version -service-mesh:v1.11.0-beta.1 \
 
 cilium hubble enable --ui
 
+# install gitops
+brew install fluxcd/tap/flux
+
 GITHUB_TOKEN=${GITHUB_TOKEN} flux bootstrap github \
   --server=https://192.168.11.100:6443 \
   --repository=homelab \
   --owner=${GITHUB_USER} \
   --path=cluster \
   --personal \
-  --cluster-domain=ruttle.net \
   --branch=master
-
-# add non-admin user
-openssl genrsa -out tom.key 4096
-openssl req -new -key tom.key -out tom.csr
-cat tom.csr | base64 | tr -d '\n'
-
-# add {{ base64_encoded_csr }} to tom-csr.yml
-
-kubectl apply -f nodes/manifests/tom-csr.yml
-kubectl certificate approve tom
-kubectl get csr tom -o jsonpath='{.status.certificate}'| base64 -d > tom.crt
-
-kubectl apply -f nodes/manifests/tom-crb.yml
-
-kubectl config set-credentials tom --client-key=tom.key --client-certificate=tom.crt --embed-certs=true
-kubectl config set-context tom --cluster=kubernetes --user=tom
 
 # kubeadm kubeconfig user ...
 
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-
-helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard
+brew install octant
--- rpi/install.sh
+++ rpi/install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-PROFILE=tom
-EMAIL_ADDRESS=tom@tomruttle.com
-SSH_IMPORT=gh:tomruttle
-
-sudo apt-get install -yq nfs-kernel-server
-
-echo "/opt/data *(rw,sync,no_root_squash,no_wdelay,no_subtree_check)" | sudo tee -a /etc/exports
-sudo exportfs -a
-sudo systemctl start nfs-kernel-server.service
-
 sudo apt-add-repository ppa:maas/3.1
 sudo apt-get install -yq maas
 
@@ -18,7 +8,4 @@ sudo apt purge snapd -yq
 
 sudo apt-get autoremove
 
-# https://picluster.ricsanfre.com/docs/os_basic/
-echo "gpu_mem=16" | sudo tee -a /boot/firmware/usercfg.txt
-
-# sudo maas createadmin --username=$PROFILE --email=$EMAIL_ADDRESS --ssh-import=$SSH_IMPORT --password=$PASSWORD
+sudo maas createadmin
