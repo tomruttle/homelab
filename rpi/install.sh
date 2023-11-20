1dd2f615cba98bdf5fd8d1dc41ba7aff8266e4a7
Tom Ruttle <tom.ruttle@thoughtworks.com>
2023-11-20 10:47:35 +0100

    Should only need the apt purge

--- rpi/install.sh
+++ rpi/install.sh
@@ -2,10 +2,7 @@
 
 sudo apt-add-repository ppa:maas/3.4-next
 sudo apt-get install -yq maas
-
-sudo snap remove lxd core18 core20 core22 bare snapd
 sudo apt purge snapd -yq
-
 sudo apt-get autoremove
 
 sudo maas createadmin
